1223486c419ab5f25cff178627da4f0573d290f8
Sakshi9598 <sakshikushwaha2018@gmail.com>
2023-10-06 14:01:52 +0530

    database file added

---
+++ userFormdb.sql
@@ -0,0 +1,13 @@
+CREATE TABLE userFormData(
+FirstName VARCHAR(10),
+LastName varchar(10),
+Email varchar(255),
+Contact VARCHAR(12),
+City VARCHAR(30)
+);
+
+USE sakshidb;
+TRUNCATE TABLE userFormData;
+
+SELECT * FROM userFormData;
+
